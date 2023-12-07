ef82643debac02d25fd15281457d135011b83ee5
tomasbelak24 <108904119+tomasbelak24@users.noreply.github.com>
2023-12-07 22:29:28 +0100

    UPDATE

--- processing_methods.py
+++ processing_methods.py
@@ -1,8 +1,9 @@
 def ZalohovaneFlaseProcessing(df):
-    df['voucher_amount'] = df['quantity'] * 0.15
+    df['voucher_amount'] = round(df['quantity'] * 0.15, 2)
     df['voucher_redemption_date'] = df['created'].apply(lambda x: x[:x.index(' ')])
     df['dummy_date'] = df['voucher_redemption_date']
     df['month'] = df['dummy_date'].apply(lambda x: x.split('.')[1])
     df['year'] = df['dummy_date'].apply(lambda x: x.split('.')[2])
     df['charity'] = 'FALSE'
-    df.drop(labels = ['cp_description', 'created', 'quantity'], axis = 1, inplace = True)
+    df.rename(columns = {'voucher':'voucher_code'}, inplace = True)
+    df.drop(labels = ['cp_description', 'created', 'quantity', 'dummy_date'], axis = 1, inplace = True)
